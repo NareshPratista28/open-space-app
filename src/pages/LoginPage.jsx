6b7de8969fb986ddef563190b849fdc0834d0723
Naresh <nareshpratista28@gmail.com>
2024-05-17 14:56:28 +0700

    make test pass

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { IoEarthOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import LoginInput from '../components/LoginInput';
-import { useDispatch } from "react-redux";
-import {asyncSetAuthUser} from "../states/authUser/action.js";
+import { asyncSetAuthUser } from '../states/authUser/action';
 
 function LoginPage() {
   const dispatch = useDispatch(); // @TODO: get dispatch function from store
@@ -16,24 +16,19 @@ function LoginPage() {
   return (
     <section className="login-page">
       <header className="login-page__hero">
-        <h1><IoEarthOutline /></h1>
+        <h1>
+          <IoEarthOutline />
+        </h1>
       </header>
       <article className="login-page__main">
         <h2>
-          See
-          {' '}
-          <strong>The World</strong>
-          ,
-          {' '}
-          <br />
-          Through Open Space.
+          See <strong>The World</strong>, <br />
+          Through Open Space App.
         </h2>
 
         <LoginInput login={onLogin} />
         <p>
-          Don&apos;t have an account?
-          {' '}
-          <Link to="/register">Register</Link>
+          Don&apos;t have an account? <Link to="/register">Register</Link>
         </p>
       </article>
     </section>
